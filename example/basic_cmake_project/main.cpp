a470342536052be42033028811cac8f982c763f5
Aymeric Pellé <aympelle@gmail.com>
2020-09-02 16:52:41 +0200

    v0.2.2:ARBA#5: Uses core-0.1.0.

--- example/basic_cmake_project/main.cpp
+++ example/basic_cmake_project/main.cpp
@@ -1,3 +1,5 @@
+#include <core/version.hpp>
+#include <core/uuid.hpp>
 #include <strn/version.hpp>
 #include <strn/string64.hpp>
 #include <strn/io.hpp>
@@ -8,6 +10,8 @@
 
 int main()
 {
+    core::uuid id("ced1cc82-37ea-450f-a5ec-00e3c10a00f2");
+    std::cout << id << std::endl;
     wgen::default_syllabary syllabary;
     strn::string64 word = syllabary.random_name64(6);
     std::cout << word << std::endl;
