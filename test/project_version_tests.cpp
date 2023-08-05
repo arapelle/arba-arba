242074b6439f5daf6c87e19eb48a57338782bd7e
Aymeric Pellé <aympelle@gmail.com>
2023-08-05 19:06:31 +0200

    v0.26.0:#22: Add unit test for generated header and version macros.

---
+++ test/project_version_tests.cpp
@@ -0,0 +1,11 @@
+#include <arba/arba/version.hpp>
+#include <gtest/gtest.h>
+#include <cstdlib>
+
+TEST(project_version_tests, test_version_macros)
+{
+    ASSERT_EQ(ARBA_ARBA_VERSION_MAJOR, 0);
+    ASSERT_EQ(ARBA_ARBA_VERSION_MINOR, 26);
+    ASSERT_EQ(ARBA_ARBA_VERSION_PATCH, 0);
+    ASSERT_STREQ(ARBA_ARBA_VERSION, "0.26.0");
+}
